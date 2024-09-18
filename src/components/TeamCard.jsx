f43854abf01b04ebf55fd6c72dbc5b4b7aacb603
이동윤 <sub09065@gmail.com>
2024-09-19 01:47:04 +0900

    Feat : TeamCard 컴포넌트 추가

---
+++ src/components/TeamCard.jsx
@@ -0,0 +1,59 @@
+import styled from "styled-components";
+import Title from "./Title";
+import TextWithIcon from "./TextWithIcon";
+import { FaRegCalendarAlt } from "react-icons/fa";
+import { BiMap } from "react-icons/bi";
+import { IoPeople } from "react-icons/io5";
+
+const TeamCardWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    border-radius: ${({theme}) => theme.size.xs};
+    border: 1px solid ${({theme}) => theme.color.black500};
+    overflow: hidden;
+    box-shadow: ${({theme}) => theme.style.shadow};
+    img{
+        top: 0;
+        width: 100%;
+        aspect-ratio: 1;
+    }
+`
+
+const InfoContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: ${({theme}) => theme.size.xs};
+    gap: ${({theme}) => theme.size.xs};
+`
+
+
+const TeamCard = ({
+    title,
+    startTime,
+    endTime,
+    location,
+    people
+
+}) => {
+    return(
+        <TeamCardWrapper>
+            <InfoContainer>
+                <Title>
+                    {title}
+                </Title>
+                <TextWithIcon IconComponent={FaRegCalendarAlt}>
+                    {startTime} - {endTime}
+                </TextWithIcon>
+                <TextWithIcon IconComponent={BiMap}>
+                    {location}
+                </TextWithIcon>
+                <TextWithIcon IconComponent={IoPeople}>
+                    {people}
+                </TextWithIcon>
+            </InfoContainer>
+        </TeamCardWrapper>
+    )
+}
+
+export default TeamCard;
