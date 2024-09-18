08e758324002bb0a7d7aa03de7ac6f8fe208c2f9
이동윤 <sub09065@gmail.com>
2024-09-19 01:46:24 +0900

    Feat : 리뷰카드 컴퍼넌트 추가

---
+++ src/components/ReviewCard.jsx
@@ -0,0 +1,77 @@
+import styled from "styled-components";
+import Title from "./Title";
+import User from "./User";
+
+const ReviewCardWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    border-radius: ${({theme}) => theme.size.xs};
+    border: 1px solid ${({theme}) => theme.color.black300};
+    overflow: hidden;
+    box-shadow: ${({theme}) => theme.style.shadow};
+
+    img{
+        top: 0;
+        width: 100%;
+        height: calc(${({theme}) => theme.size.xxl} + ${({theme}) => theme.size.xl});
+   
+    }
+`
+const UserWrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+    width : fit-content;
+    border: none;
+    font-size: ${({theme}) => theme.fontSize.base};
+    gap: ${({theme}) => theme.size.xs};
+    padding-top: ${({theme}) => theme.size.xs};
+    padding-left: ${({theme}) => theme.size.xs};
+    img {
+        top: 0;
+        border-radius: ${({theme}) => theme.size.md};
+        border: 1px solid ${({theme}) => theme.color.black300};
+        width: ${({theme}) => theme.size.md};
+        height: ${({theme}) => theme.size.md};
+        aspect-ratio: 1;
+    }
+`
+const InfoContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: ${({theme}) => theme.size.xs};
+    gap: ${({theme}) => theme.size.xs};
+    font-size: ${({theme}) => theme.fontSize.sm};
+`
+
+
+const ReviewCard = ({
+    src,
+    user_src,
+    user_id,
+    title,
+    content
+
+}) => {
+    return(
+        <ReviewCardWrapper>
+            <img src={src}/>
+            <UserWrapper>
+            <User {...
+            {
+                user_src:"https://cdn2.colley.kr/colley_content_paragraph_14_145_160030797214320.jpg",
+                user_id: "Heydy"
+            }
+            }/>
+            </UserWrapper>
+            <InfoContainer>
+                <Title>
+                    {title}
+                </Title>
+                {content}
+            </InfoContainer>
+        </ReviewCardWrapper>
+    )
+}
+
+export default ReviewCard;
