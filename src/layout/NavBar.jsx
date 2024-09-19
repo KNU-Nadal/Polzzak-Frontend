89b617f66ab7ae54e121152a7caabd5183835545
MOJAN <sjs732@naver.com>
2024-09-19 21:07:11 +0900

    Feat: Navbar 이동 기능, 디자인 일부 수정

--- src/layout/NavBar.jsx
+++ src/layout/NavBar.jsx
@@ -4,13 +4,17 @@ import { RiHome2Line } from "react-icons/ri";
 import { GrLocation } from "react-icons/gr";
 import { FaRegUser } from "react-icons/fa6";
 import { LuSettings2 } from "react-icons/lu";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const NavBarWrapper = styled.div`
+    position: sticky;
+    bottom: 0;
     display: flex;
     justify-content: space-between;
-    flex-direction: col;
+    border-top: 1px solid ${({theme}) => theme.color.black400};
+    background-color: ${({theme}) => theme.color.bg};
     width: 100%;
-    padding: ${({theme}) => theme.size.xs} ${({theme}) => theme.size.base};
+    padding: ${({theme}) => theme.size.sm} ${({theme}) => theme.size.md};
 
 `
 
@@ -18,38 +22,62 @@ const NavBarIconWrapper = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    align-items: center;
     gap: ${({theme}) => theme.size.xxs};
     font-size: ${({theme}) => theme.fontSize.sm};
+    font-weight: ${(props) => props.isSelected ? 700: 400};
+    color: ${(props) => props.isSelected ? ({theme}) => theme.color.primary : ({theme}) => theme.color.black900};
+    cursor: ${(props) => props.isSelected ? "default" : "pointer"};
 `
 const NavBarIcon = ({
-    color,
+    isSelected,
     IconComponent,
     children,
+    path,
 }) => {
+    const navigate = useNavigate();
     return(
-        <NavBarIconWrapper>
-            <IconComponent style={{fontSize: theme.size.lg, flexShrink: 0}} color={theme.color.black900}/>
+        <NavBarIconWrapper isSelected={isSelected} onClick={() => {isSelected ? null : navigate(path)}}>
+            <IconComponent style={{fontSize: theme.size.lg, flexShrink: 0}} color={isSelected ? theme.color.primary : theme.color.black900}/>
             {children}
         </NavBarIconWrapper>
     )
 }
 
 const NavBar = ( ) => {
+    const { pathname } = useLocation();
+    const ButtonInfoList = [
+        {
+            icon: RiHome2Line,
+            path: "/",
+            string: "메인 메뉴"
+        },
+        {
+            icon: GrLocation,
+            path: "/map",
+            string: "지도 보기"
+        },
+        {
+            icon: FaRegUser,
+            path: "/myplogging",
+            string: "내 플로깅"
+        },
+        {
+            icon: LuSettings2,
+            path: "/setting",
+            string: "환경 설정"
+        }
+    ]
     return(
         <NavBarWrapper>
-                <NavBarIcon IconComponent={RiHome2Line}>
-                    {"메인 메뉴"}
-                </NavBarIcon>
-                <NavBarIcon IconComponent={GrLocation}>
-                    {"지도 보기"}
-                </NavBarIcon>
-                <NavBarIcon IconComponent={FaRegUser}>
-                    {"내 플로깅"}
-                </NavBarIcon>
-                <NavBarIcon IconComponent={LuSettings2}>
-                    {"환경설정"}
-                </NavBarIcon>
+            {
+                ButtonInfoList.map((ButtonInfo) => {
+                    return(
+                        <NavBarIcon isSelected={ButtonInfo.path == pathname} IconComponent={ButtonInfo.icon} path={ButtonInfo.path}>
+                            {ButtonInfo.string}
+                        </NavBarIcon>
+                    )
+                })
+            }
         </NavBarWrapper>
     )
 }
