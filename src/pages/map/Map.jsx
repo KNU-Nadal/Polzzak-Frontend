cdf136b92648077eb3acbaa1a2c829aa3f965f4b
이동윤 <sub09065@gmail.com>
2024-09-21 03:23:55 +0900

    Feat : PopUp 컴포넌트 테스트

--- src/pages/map/Map.jsx
+++ src/pages/map/Map.jsx
@@ -1,8 +1,15 @@
+import PopUp from "./PopUp";
+
+const PopUpObj = {
+    src:"https://cc-prod.scene7.com/is/image/CCProdAuthor/Photography%20Page%20JDI_marquee_tb?$pjpeg$&jpegSize=300&wid=1536",
+    title:"카페 나달",
+    location:"대구광역시 북구 대학로 80",
+    date: "2024/09/20 18:00 - 2024/09/20 20:00" 
+}
+
 const Map = () => {
     return(
-        <>
-            Map 페이지 입니다.
-        </>
+            <PopUp {...PopUpObj} />
     )
 }
 
