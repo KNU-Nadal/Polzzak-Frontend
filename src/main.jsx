bb11e69ff1ec8862cede831bcc19a0247fe3b580
MOJAN <sjs732@naver.com>
2024-09-22 03:33:44 +0900

    Feat: RecoilRoot

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { RecoilRoot } from 'recoil'
 
 createRoot(document.getElementById('root')).render(
-    <App />
+    <RecoilRoot>
+        <App />
+    </RecoilRoot>
 )
