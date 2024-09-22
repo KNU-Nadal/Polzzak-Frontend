fd9e2043456f52e435f1f058eabf1ca14b02a561
이동윤 <sub09065@gmail.com>
2024-09-22 12:37:13 +0900

    Feat : 일단 오류 안나게 parmeter 이름만 수정

--- src/pages/myplogging/MyPlogging.jsx
+++ src/pages/myplogging/MyPlogging.jsx
@@ -80,98 +80,98 @@ const MyPlogging = () => {
     return (
         <MainWrapper>
             <Title>
-                내가 등록한 이벤트
+                내가 참가하는 이벤트
             </Title>
             <MyEventCardContainer>
                 <EventCard {...
                     {
-                        src:"https://cdn.welfarehello.com/naver-blog/production/buk_daegu/2024-09/223582886882/buk_daegu_223582886882_1.png",
+                        image_name:"https://cdn.welfarehello.com/naver-blog/production/buk_daegu/2024-09/223582886882/buk_daegu_223582886882_1.png",
                         title:"금호강 바람소리길 축제",
-                        startTime:"09.28",
-                        endTime:"09.29",
-                        location:"금호강 산격대교 하단일원(산격야영장)"
+                        start_time:"09.28",
+                        end_time:"09.29",
+                        address:"금호강 산격대교 하단일원(산격야영장)"
                     }
                 }/>
                 <EventCard {...
                     {
-                        src:"https://cdn.welfarehello.com/naver-blog/production/buk_daegu/2024-09/223582886882/buk_daegu_223582886882_1.png",
+                        image_name:"https://cdn.welfarehello.com/naver-blog/production/buk_daegu/2024-09/223582886882/buk_daegu_223582886882_1.png",
                         title:"금호강 바람소리길 축제",
-                        startTime:"09.28",
-                        endTime:"09.29",
-                        location:"금호강 산격대교 하단일원(산격야영장)"
+                        start_time:"09.28",
+                        end_time:"09.29",
+                        address:"금호강 산격대교 하단일원(산격야영장)"
                     }
                 }/>
                 <EventCard {...
                     {
-                        src:"https://cdn.welfarehello.com/naver-blog/production/buk_daegu/2024-09/223582886882/buk_daegu_223582886882_1.png",
+                        image_name:"https://cdn.welfarehello.com/naver-blog/production/buk_daegu/2024-09/223582886882/buk_daegu_223582886882_1.png",
                         title:"금호강 바람소리길 축제",
-                        startTime:"09.28",
-                        endTime:"09.29",
-                        location:"금호강 산격대교 하단일원(산격야영장)"
+                        start_time:"09.28",
+                        end_time:"09.29",
+                        address:"금호강 산격대교 하단일원(산격야영장)"
                     }
                 }/>
                 <EventCard {...
                     {
-                        src:"https://cdn.welfarehello.com/naver-blog/production/buk_daegu/2024-09/223582886882/buk_daegu_223582886882_1.png",
+                        image_name:"https://cdn.welfarehello.com/naver-blog/production/buk_daegu/2024-09/223582886882/buk_daegu_223582886882_1.png",
                         title:"금호강 바람소리길 축제",
-                        startTime:"09.28",
-                        endTime:"09.29",
-                        location:"금호강 산격대교 하단일원(산격야영장)"
+                        start_time:"09.28",
+                        end_time:"09.29",
+                        address:"금호강 산격대교 하단일원(산격야영장)"
                     }
                 }/>
 
             </MyEventCardContainer>
 
             <Title>
-                내가 등록한 플로깅
+                내가 참가하는 플로깅
             </Title>
             <MyTeamCardContainer>
             <MyTeamCard {...
                 {
-                    user_src:"https://velog.velcdn.com/images/0912078/post/11d186c1-58d3-4461-9709-4d0bb2914536/image.jpg",
+                    profile_image:"https://velog.velcdn.com/images/0912078/post/11d186c1-58d3-4461-9709-4d0bb2914536/image.jpg",
                     title: "신천 플로깅 (중,고수 환영)🙋‍♂️",
-                    startTime: "9월 18일 20시",
-                    location: "대현동 부근"
+                    start_time: "9월 18일 20시",
+                    place_name: "대현동 부근"
                 }
             }/>
             <MyTeamCard {...
                 {
-                    user_src:"https://velog.velcdn.com/images/0912078/post/11d186c1-58d3-4461-9709-4d0bb2914536/image.jpg",
+                    profile_image:"https://velog.velcdn.com/images/0912078/post/11d186c1-58d3-4461-9709-4d0bb2914536/image.jpg",
                     title: "신천 플로깅 (중,고수 환영)🙋‍♂️",
-                    startTime: "9월 18일 20시",
-                    location: "대현동 부근"
+                    start_time: "9월 18일 20시",
+                    place_name: "대현동 부근"
                 }
             }/>
                         <MyTeamCard {...
                 {
-                    user_src:"https://velog.velcdn.com/images/0912078/post/11d186c1-58d3-4461-9709-4d0bb2914536/image.jpg",
+                    profile_image:"https://velog.velcdn.com/images/0912078/post/11d186c1-58d3-4461-9709-4d0bb2914536/image.jpg",
                     title: "신천 플로깅 (중,고수 환영)🙋‍♂️",
-                    startTime: "9월 18일 20시",
-                    location: "대현동 부근"
+                    start_time: "9월 18일 20시",
+                    place_name: "대현동 부근"
                 }
             }/>
                         <MyTeamCard {...
                 {
-                    user_src:"https://velog.velcdn.com/images/0912078/post/11d186c1-58d3-4461-9709-4d0bb2914536/image.jpg",
+                    profile_image:"https://velog.velcdn.com/images/0912078/post/11d186c1-58d3-4461-9709-4d0bb2914536/image.jpg",
                     title: "신천 플로깅 (중,고수 환영)🙋‍♂️",
-                    startTime: "9월 18일 20시",
-                    location: "대현동 부근"
+                    start_time: "9월 18일 20시",
+                    place_name: "대현동 부근"
                 }
             }/>
                                     <MyTeamCard {...
                 {
-                    user_src:"https://velog.velcdn.com/images/0912078/post/11d186c1-58d3-4461-9709-4d0bb2914536/image.jpg",
+                    profile_image:"https://velog.velcdn.com/images/0912078/post/11d186c1-58d3-4461-9709-4d0bb2914536/image.jpg",
                     title: "신천 플로깅 (중,고수 환영)🙋‍♂️",
-                    startTime: "9월 18일 20시",
-                    location: "대현동 부근"
+                    start_time: "9월 18일 20시",
+                    place_name: "대현동 부근"
                 }
             }/>
                                     <MyTeamCard {...
                 {
-                    user_src:"https://velog.velcdn.com/images/0912078/post/11d186c1-58d3-4461-9709-4d0bb2914536/image.jpg",
+                    profile_image:"https://velog.velcdn.com/images/0912078/post/11d186c1-58d3-4461-9709-4d0bb2914536/image.jpg",
                     title: "신천 플로깅 (중,고수 환영)🙋‍♂️",
-                    startTime: "9월 18일 20시",
-                    location: "대현동 부근"
+                    start_time: "9월 18일 20시",
+                    place_name: "대현동 부근"
                 }
             }/>
             </MyTeamCardContainer>
@@ -183,21 +183,21 @@ const MyPlogging = () => {
             <ReviewCardMainContainer>
                 <ReviewCardMain {...
                     {
-                        src:"https://png.pngtree.com/thumb_back/fh260/background/20210902/pngtree-blue-sky-and-white-clouds-pure-sky-image_786707.jpg",
+                        image_name:"https://png.pngtree.com/thumb_back/fh260/background/20210902/pngtree-blue-sky-and-white-clouds-pure-sky-image_786707.jpg",
                         title:"드디어 찾아온 가을,  신천 플로깅 어때요?",
                         content:"길었던 여름의 뜨거운 열기가 지나가고, 시원한 가을 바람이 불어오고 있네요. 오늘의 후기는 9월 20일에 진행했던 신천 플로깅 후기입니다! 이번 플로깅은 경북대학교..."
                     }
                 }/>     
                 <ReviewCardMain {...
                     {
-                        src:"https://png.pngtree.com/thumb_back/fh260/background/20210902/pngtree-blue-sky-and-white-clouds-pure-sky-image_786707.jpg",
+                        image_name:"https://png.pngtree.com/thumb_back/fh260/background/20210902/pngtree-blue-sky-and-white-clouds-pure-sky-image_786707.jpg",
                         title:"드디어 찾아온 가을,  신천 플로깅 어때요?",
                         content:"길었던 여름의 뜨거운 열기가 지나가고, 시원한 가을 바람이 불어오고 있네요. 오늘의 후기는 9월 20일에 진행했던 신천 플로깅 후기입니다! 이번 플로깅은 경북대학교..."
                     }
                 }/>
                 <ReviewCardMain {...
                     {
-                        src:"https://png.pngtree.com/thumb_back/fh260/background/20210902/pngtree-blue-sky-and-white-clouds-pure-sky-image_786707.jpg",
+                        image_name:"https://png.pngtree.com/thumb_back/fh260/background/20210902/pngtree-blue-sky-and-white-clouds-pure-sky-image_786707.jpg",
                         title:"드디어 찾아온 가을,  신천 플로깅 어때요?",
                         content:"길었던 여름의 뜨거운 열기가 지나가고, 시원한 가을 바람이 불어오고 있네요. 오늘의 후기는 9월 20일에 진행했던 신천 플로깅 후기입니다! 이번 플로깅은 경북대학교..."
                     }
