1d447188ff39936cc34c614dd76af6853c54c9ae
이동윤 <sub09065@gmail.com>
2024-09-22 12:33:38 +0900

    Feat : API 연결 안된 컴포넌트 임시 삭제

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -199,45 +199,6 @@ const Main = () => {
                 }
             }/>
 
-        <MyEventCardContainer>
-                <EventCard {...
-                    {
-                        src:"https://cdn.welfarehello.com/naver-blog/production/buk_daegu/2024-09/223582886882/buk_daegu_223582886882_1.png",
-                        title:"금호강 바람소리길 축제",
-                        startTime:"09.28",
-                        endTime:"09.29",
-                        location:"금호강 산격대교 하단일원(산격야영장)"
-                    }
-                }/>
-                <EventCard {...
-                    {
-                        src:"https://cdn.welfarehello.com/naver-blog/production/buk_daegu/2024-09/223582886882/buk_daegu_223582886882_1.png",
-                        title:"금호강 바람소리길 축제",
-                        startTime:"09.28",
-                        endTime:"09.29",
-                        location:"금호강 산격대교 하단일원(산격야영장)"
-                    }
-                }/>
-                <EventCard {...
-                    {
-                        src:"https://cdn.welfarehello.com/naver-blog/production/buk_daegu/2024-09/223582886882/buk_daegu_223582886882_1.png",
-                        title:"금호강 바람소리길 축제",
-                        startTime:"09.28",
-                        endTime:"09.29",
-                        location:"금호강 산격대교 하단일원(산격야영장)"
-                    }
-                }/>
-                <EventCard {...
-                    {
-                        src:"https://cdn.welfarehello.com/naver-blog/production/buk_daegu/2024-09/223582886882/buk_daegu_223582886882_1.png",
-                        title:"금호강 바람소리길 축제",
-                        startTime:"09.28",
-                        endTime:"09.29",
-                        location:"금호강 산격대교 하단일원(산격야영장)"
-                    }
-                }/>
-
-            </MyEventCardContainer>
 
                 <Footer/>
         </MainWrapper>
